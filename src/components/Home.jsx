6ca2ce370887104b6184b427ed2815803a863c53
leonirmalraj <leo@cloudrevelinnovation.com>
2024-04-04 18:30:52 +0530

    home page responsive issue fixed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,27 @@
 import React from 'react'
 import '../assets/css/Home.css'
 import Carousel from 'react-bootstrap/Carousel';
-import banner_one from "../assets/images/banner_one.jpg";
-import banner_two from "../assets/images/banner_two.jpg";
+import banner_one from "../assets/images/banner_one.png";
+import banner_two from "../assets/images/banner_two.png";
+import banner_three from "../assets/images/banner_three.png";
+import banner_four from "../assets/images/banner_four.png";
+// import banner_five from "../assets/images/banner_five.png";
 const Home = () => {
   return (
     <div className="home-wrapper">
       <Carousel data-bs-theme="dark">
-        <Carousel.Item>
+        {/* <Carousel.Item>
           <div className='carousel_image'>
-            <img className="img" src={banner_one} alt="First slide" />
+            <img className="img" src="https://suncitycinemas.com/new_website/asset/error/2.jpg" alt="First slide" />
+          </div>
+          <Carousel.Caption>
+            <h5>Embrace colors that effortlessly blend into the natural landscape.</h5>
+            <p></p>
+          </Carousel.Caption>
+        </Carousel.Item> */}
+        <Carousel.Item>
+          <div className="carousel_image">
+            <img className="img" src={banner_two} alt="First slide" />
           </div>
           <Carousel.Caption>
             <h5>Embrace colors that effortlessly blend into the natural landscape.</h5>
@@ -18,7 +30,25 @@ const Home = () => {
         </Carousel.Item>
         <Carousel.Item>
           <div className="carousel_image">
-            <img className="img" src={banner_two} alt="First slide" />
+            <img className="img" src={banner_three} alt="First slide" />
+          </div>
+          <Carousel.Caption>
+            <h5>Embrace colors that effortlessly blend into the natural landscape.</h5>
+            <p></p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <div className="carousel_image">
+            <img className="img" src={banner_four} alt="First slide" />
+          </div>
+          <Carousel.Caption>
+            <h5>Embrace colors that effortlessly blend into the natural landscape.</h5>
+            <p></p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <div className="carousel_image">
+            <img className="img" src={banner_one} alt="First slide" />
           </div>
           <Carousel.Caption>
             <h5>Embrace colors that effortlessly blend into the natural landscape.</h5>
